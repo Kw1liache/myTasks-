58a862d1dbcc041ccd2b0e4e5f7a9a41a3b1efd4
Kw1liache <zagoskinmaxii@yandex.ru>
2025-10-12 22:43:53 +0500

    HistogrammaUlern

---
+++ HistogramTask.cs
@@ -0,0 +1,27 @@
+﻿namespace Names;
+
+internal static class HistogramTask
+{
+    public static HistogramData GetBirthsPerDayHistogram(NameData[] names, string name)
+    {
+        var birthsPerDay = new double[31];
+        foreach (var person in names)
+        {
+            if (person.Name == name && person.BirthDate.Day != 1)
+            {
+                birthsPerDay[person.BirthDate.Day - 1]++;
+            }
+        }
+
+        var days = new string[31];
+        for (var i = 0; i < 31; i++)
+        {
+            days[i] = (i + 1).ToString();
+        }
+        
+        return new HistogramData(
+            $"Рождаемость людей с именем '{name}'", 
+            days, 
+            birthsPerDay);
+    }
+}
