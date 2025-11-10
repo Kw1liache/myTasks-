2e6fdb1469d7922cf609d2aff28326267d94791d
Kw1liache <zagoskinmaxii@yandex.ru>
2025-11-10 22:51:03 +0500

    Create Left Border Task.cs

---
+++ Left Border Task.cs
@@ -0,0 +1,23 @@
+/*Во многих программах в разных контекстах можно увидеть функцию автодополнения вводимого текста. 
+Обычно это работает так: есть словарь всех допустимых значений, 
+и когда пользователь вводит начало некоторого слова, 
+ему показывают несколько подходящих слов из словаря, 
+начинающихся с букв, уже введенных пользователем.*/
+using System;
+using System.Collections.Generic;
+using System.Linq;
+namespace Autocomplete;
+
+public class LeftBorderTask
+{
+	public static int GetLeftBorderIndex(IReadOnlyList<string> phrases, string prefix, int left, int right)
+	{
+		if (left + 1 >= right)
+			return left;
+		var middle = (left + right) / 2;
+		if (string.Compare(phrases[middle], prefix, StringComparison.InvariantCultureIgnoreCase) < 0)
+			return GetLeftBorderIndex(phrases, prefix, middle, right);
+		else
+			return GetLeftBorderIndex(phrases, prefix, left, middle);
+	}
+}
