a42e226f15ad71eec29c51abb9be5b41f493e5bf
Kw1liache <zagoskinmaxii@yandex.ru>
2025-11-11 19:44:47 +0500

    Create Right Border Task.cs

---
+++ Right Border Task.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Autocomplete;
+
+public class RightBorderTask
+{
+	public static int GetRightBorderIndex(IReadOnlyList<string> phrases, string prefix, int left, int right)
+	{
+		while (left + 1 < right)
+		{
+			var middle = (right + left) / 2;
+			if (string.Compare(phrases[middle], prefix, StringComparison.InvariantCultureIgnoreCase) <= 0 
+			    || phrases[middle].StartsWith(prefix, StringComparison.InvariantCultureIgnoreCase))
+				left = middle;
+			else 
+				right = middle;
+		}
+		return right;
+	}
+}
